2a0888286c50be254ec5c3ae86e04e9e0061d881
Piyus Pradhan <kumarpradhanpiyush77@gmail.com>
2024-10-12 05:54:26 +0530

    added form for the search.

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -4,14 +4,19 @@ import { SearchContext } from '../context/SearchContext';
 import backImage from '../assets/images/back.png';
 import searchImage from '../assets/images/search.png'
 
-const Navbar = ({movies, handleSetFilteredMovies}) => {
+const Navbar = ({ movies, handleSetFilteredMovies }) => {
     const { searchTerm, setSearchTerm } = useContext(SearchContext);
 
     const handleBack = () => {
         window.history.back(); // Go back to the previous page
     };
 
-    const handleSearch = () => {
+    const handleSearch = (event) => {
+        event.preventDefault();
+        if (searchTerm.length < 3 || searchTerm.length > 10) {
+            alert('Please enter between 3 and 10 characters to search.');
+            return;
+        }
         handleSetFilteredMovies(movies.filter((movie) => {
             return movie.name.toLowerCase().includes(searchTerm.toLowerCase())
         }))
@@ -25,14 +30,18 @@ const Navbar = ({movies, handleSetFilteredMovies}) => {
             <button className="nav-button back-button" onClick={handleBack}>
                 <img src={backImage} alt='back'></img>
             </button>
-            <input
-                type="text"
-                placeholder=""
-                className="search-input"
-                value={searchTerm}
-                onChange={handleChange} // Update search term on input change
-            />
-            <button className="nav-button search-button" onClick={handleSearch}><img src={searchImage} alt='search'></img></button>
+            <form className='form'>
+                <input
+                    type="text"
+                    placeholder=""
+                    className="search-input"
+                    value={searchTerm}
+                    onChange={handleChange} // Update search term on input change
+                    minLength={3}
+                />
+                <button type='submit' className="nav-button search-button" onClick={handleSearch}><img src={searchImage} alt='search'></img></button>
+            </form>
+
         </nav>
     );
 }
