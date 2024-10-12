be2d51b24baa7ae85b41ab16ceb514a579653485
Piyus Pradhan <kumarpradhanpiyush77@gmail.com>
2024-10-12 20:32:45 +0530

    updated the navbar UI.

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -5,6 +5,8 @@ import backImage from '../assets/images/back.png';
 import searchImage from '../assets/images/search.png';
 import closeImage from '../assets/images/close.png';
 import navbarImage from '../assets/images/navbar.png';
+import Grid from '@mui/material/Grid2';
+import { IconButton, Box, Typography } from '@mui/material';
 
 const Navbar = ({ movies, handleSetFilteredMovies }) => {
     const [searchBool, setSearchBool] = useState(false);
@@ -59,37 +61,58 @@ const Navbar = ({ movies, handleSetFilteredMovies }) => {
 
     return (
         <>
-        {/* Navbar */}
-            <nav className={`navbar ${isNavbarActive ? '' : 'hidden'}`}>
-                
-                <button className="nav-button back-button" onClick={handleBack}>
-                    <img src={backImage} alt='back' />
-                </button>
-                <div className="page-title">
-                    <h2>
+            {/* Navbar */}
+            <nav className={`${isNavbarActive ? '' : 'hidden'}`} style={{ display: 'flex' }}>
+                <IconButton className="nav-button back-button" onClick={handleBack}>
+                    <img
+                        src={backImage}
+                        alt='back'
+                        style={{ width: '30px', height: '30px' }} // Adjust the size as needed
+                    />
+                </IconButton>
+                <Box
+                    className="page-title"
+                    sx={{
+                        flexGrow: 1, // Allow the Box to grow
+                        flexShrink: 0, // Prevent shrinking
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
+                >
+                    <Typography variant="h5"> {/* Adjust the variant as needed */}
                         Romantic Comedy
-                    </h2>
-                </div>
-                <form className='form' onSubmit={handleSearch}>
+                    </Typography>
+                </Box>
+                <form className='form' onSubmit={handleSearch} style={{ display: 'flex', alignItems: 'center' }}>
                     <input
                         type="text"
                         placeholder="Search..."
                         className="search-input"
                         value={searchTerm}
-                        onChange={handleChange} // Update search term on input change
+                        onChange={handleChange}
                         minLength={3}
                     />
                     {!searchBool && (
-                        <button type='submit' className="nav-button search-button">
-                            <img src={searchImage} alt='search' />
-                        </button>
+                        <IconButton type='submit' className="nav-button search-button">
+                            <img
+                                src={searchImage}
+                                alt='search'
+                                style={{ width: '30px', height: '30px' }} // Adjust the size as needed
+                            />
+                        </IconButton>
                     )}
                     {searchBool && (
-                        <button type='button' className="nav-button search-button" onClick={handleClose}>
-                            <img src={closeImage} alt='close' />
-                        </button>
+                        <IconButton type='button' className="nav-button search-button" onClick={handleClose}>
+                            <img
+                                src={closeImage}
+                                alt='close'
+                                style={{ width: '30px', height: '30px' }} // Adjust the size as needed
+                            />
+                        </IconButton>
                     )}
                 </form>
+
             </nav>
 
             {/* Scroll Placeholder */}
