54463d77e6190b75ddc3aa43da6a1628aba69df7
Piyus Pradhan <kumarpradhanpiyush77@gmail.com>
2024-10-12 06:39:33 +0530

    handled close button.

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,10 +1,12 @@
 import './navbar.css';
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SearchContext } from '../context/SearchContext';
 import backImage from '../assets/images/back.png';
 import searchImage from '../assets/images/search.png'
+import closeImage from '../assets/images/close.png'
 
 const Navbar = ({ movies, handleSetFilteredMovies }) => {
+    const [ searchBool, setSearchBool ] = useState(true);
     const { searchTerm, setSearchTerm } = useContext(SearchContext);
 
     const handleBack = () => {
@@ -17,9 +19,17 @@ const Navbar = ({ movies, handleSetFilteredMovies }) => {
             alert('Please enter between 3 and 10 characters to search.');
             return;
         }
+        setSearchBool(true);
         handleSetFilteredMovies(movies.filter((movie) => {
             return movie.name.toLowerCase().includes(searchTerm.toLowerCase())
         }))
+    };
+
+    const handleClose = (event) => {
+        event.preventDefault();
+        setSearchTerm("");
+        setSearchBool(false);
+        handleSetFilteredMovies(movies);
     };
 
     const handleChange = (event) => {
@@ -30,7 +40,7 @@ const Navbar = ({ movies, handleSetFilteredMovies }) => {
             <button className="nav-button back-button" onClick={handleBack}>
                 <img src={backImage} alt='back'></img>
             </button>
-            <form className='form'>
+            <form className='form' onSubmit={handleSearch}>
                 <input
                     type="text"
                     placeholder=""
@@ -39,7 +49,8 @@ const Navbar = ({ movies, handleSetFilteredMovies }) => {
                     onChange={handleChange} // Update search term on input change
                     minLength={3}
                 />
-                <button type='submit' className="nav-button search-button" onClick={handleSearch}><img src={searchImage} alt='search'></img></button>
+                {!searchBool && <button type='submit' className="nav-button search-button"><img src={searchImage} alt='search'></img></button>}
+                {searchBool && <button type='button' className="nav-button search-button" onClick={handleClose}><img src={closeImage} alt='close'></img></button>}
             </form>
 
         </nav>
